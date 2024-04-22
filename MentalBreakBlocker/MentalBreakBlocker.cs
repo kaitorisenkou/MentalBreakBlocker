46b01bdd017a5a78dba98d15b4a62030263b9336
kaitorisenkou <kaitorisenkou@icloud.com>
2024-04-22 15:56:36 +0900

    プロジェクト ファイルを追加します。

---
+++ MentalBreakBlocker/MentalBreakBlocker.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace MentalBreakBlocker {
+    [StaticConstructorOnStartup]
+    public class MentalBreakBlocker {
+        static MentalBreakBlocker() {
+            Log.Message("[MentalBreakBlocker] Now active");
+            var harmony = new Harmony("kaitorisenkou.MentalBreakBlocker");
+            
+            harmony.Patch(
+                AccessTools.Method(typeof(MentalStateHandler), nameof(MentalStateHandler.TryStartMentalState), null, null),
+                new HarmonyMethod(typeof(MentalBreakBlocker), nameof(Patch_TryStartMentalState), null),
+                null,
+                null,
+                null
+                );
+            Log.Message("[MentalBreakBlocker] Harmony patch complete!");
+        }
+
+        public static bool Patch_TryStartMentalState(ref bool __result, Pawn ___pawn, MentalStateDef stateDef, bool causedByMood, bool causedByDamage,bool causedByPsycast) {
+            if (___pawn == null)
+                return true;
+            foreach (Hediff hediff in ___pawn.health.hediffSet.hediffs) {
+                var ex = hediff.def.GetModExtension<ModExtension_MentalBreakBlocker>();
+                if (ex != null) {
+                    if (ex.IsBlocked(causedByMood, causedByDamage, causedByPsycast)) {
+                        __result = false;
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ MentalBreakBlocker/ModExtension_MentalBreakBlocker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace MentalBreakBlocker {
+    public class ModExtension_MentalBreakBlocker : DefModExtension {
+        public BlockMentalBreakCause cause = BlockMentalBreakCause.all;
+
+        public bool IsBlocked(bool causedByMood, bool causedByDamage, bool causedByPsycast) {
+            return
+                (((byte)cause) & (
+                (causedByMood ? 1 : 0) | (causedByDamage ? 2 : 0) | (causedByPsycast ? 4 : 0))
+                ) > 0;
+        }
+    }
+    public enum BlockMentalBreakCause : byte {
+        mood = 1,
+        damage = 2,
+        psycast = 4,
+
+        moodAndDamage = 3,
+        moodAndPsycast = 5,
+        damageAndPsycast = 6,
+
+        all = 7
+    }
+}
