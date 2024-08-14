fdce6c9e33d6cf99f78252eec9cbf3a3ef6fbcec
kaitorisenkou <kaitorisenkou@icloud.com>
2024-08-15 02:08:51 +0900

    add <isWhitelist>

--- MentalBreakBlocker/ModExtension_MentalBreakBlocker.cs
+++ MentalBreakBlocker/ModExtension_MentalBreakBlocker.cs
@@ -9,15 +9,17 @@ using Verse;
 namespace MentalBreakBlocker {
     public class ModExtension_MentalBreakBlocker : DefModExtension {
         public BlockMentalBreakCause cause = BlockMentalBreakCause.all;
+        public bool isWhitelist = false;
 
         public bool IsBlocked(bool causedByMood, bool causedByDamage, bool causedByPsycast) {
-            return
-                (((byte)cause) & (
-                (causedByMood ? 1 : 0) | (causedByDamage ? 2 : 0) | (causedByPsycast ? 4 : 0))
-                ) > 0;
+            byte argByte = (byte)((causedByMood ? 1 : 0) | (causedByDamage ? 2 : 0) | (causedByPsycast ? 4 : 0));
+            return (((byte)cause) & argByte) > 0
+                ^ isWhitelist;
         }
     }
     public enum BlockMentalBreakCause : byte {
+        anyway = 0,
+
         mood = 1,
         damage = 2,
         psycast = 4,
